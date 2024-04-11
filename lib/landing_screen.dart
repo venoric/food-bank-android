7c69af99112eb108ad913d51b786c03b8432e377
Dominic Andrew Bernardino <dominic-andrew.bernardino.381@my.csun.edu>
2024-04-10 21:27:47 -0700

    set launcher icon and add logo to landing screen

--- lib/landing_screen.dart
+++ lib/landing_screen.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 
 import 'login_screen.dart';
@@ -15,38 +16,50 @@ class _LandingScreenState extends State<LandingScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text('FoodBank')),
-        body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                ElevatedButton(
-                  child: const Text('Login'),
-                  onPressed: () {
-                    // Go to the login screen
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const LoginScreen()),
-                    );
-                  },
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                ElevatedButton(
-                  child: const Text('Register'),
-                  onPressed: () {
-                    // Go to the registration screen
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const RegistrationScreen()),
-                    );
-                  },
-                ),
-              ],
-            )
+      appBar: AppBar(title: const Text('Welcome to FoodBank')),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 100.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Image.asset(
+                'assets/icon/foodbank_logo.png',
+                width: 200,
+                height: 200,
+                fit: BoxFit.contain,
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              ElevatedButton(
+                child: const Text('Login'),
+                onPressed: () {
+                  // Go to the login screen
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const LoginScreen()),
+                  );
+                },
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              ElevatedButton(
+                child: const Text('Register'),
+                onPressed: () {
+                  // Go to the registration screen
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const RegistrationScreen()),
+                  );
+                },
+              ),
+            ],
+          ),
         )
+      )
     );
   }
 }
